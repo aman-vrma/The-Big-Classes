4e971149167c197081d6ea5f192d19ecb0620eb6
Aman Verma <piikaachuugaming@gmail.com>
2026-09-13 00:33:03 +0530

    fix: restore firebase config

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,19 +1,21 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyDgjJpqGQsT8khvXX7M-th3OVQ-HYXzgzM",
+  authDomain: "the-big-classes.firebaseapp.com",
+  projectId: "the-big-classes",
+  storageBucket: "the-big-classes.firebasestorage.app",
+  messagingSenderId: "874746583485",
+  appId: "1:874746583485:web:0af64339d97aa82240dad4",
+  measurementId: "G-TRF3G4YGTZ"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Export auth to use in login/signup components
-export const auth = getAuth(app);
-export default app;
+const analytics = getAnalytics(app);
